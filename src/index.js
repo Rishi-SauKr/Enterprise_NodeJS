32d27b29cdef0dcd2f8daa8553b0ae6f30b9eb8d
“Rishi-SauKr” <riderishi22@gmail.com>
2025-05-01 18:20:22 +0530

    Added Foreign Key Migration File for one to many

--- src/index.js
+++ src/index.js
@@ -14,10 +14,34 @@ app.use(express.json());
 //     res.status(StatusCodes.OK).json("body value printed");
 // })
 app.use("/api", apiRoutes);
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(
         `Successfully started the server on PORT : ${ServerConfig.PORT}`
     );
+    const { City, Airport } = require("./models");
+    // const bengaluru = await City.findByPk(2);
+    // console.log(bengaluru);
+    // const airport = await Airport.create({
+    //     name: "Kempegowda Airport", code: 'BLR', cityId: 1
+    // });
+    // const KMPAirport = await bengaluru.createAirport({ name: 'Kempegowda Airport', code: 'BLR' });
+    // console.log(KMPAirport);
+    // const HubliAirport = await bengaluru.createAirport({ name: 'Bengaluru International Airport', code: 'HBL' });
+    // const airportsInBlr = await bengaluru.getAirports();
+    // console.log(airportsInBlr);
+    /** This Errors out. */
+    // const hblAirport = await Airport.findByPk(7);
+    // console.log(hblAirport);
+    // await bengaluru.removeAirport(hblAirport);
+
+
+    // const city = await City.findByPk(3);
+    // await city.createAirport({ name: 'Indore Airport', code: 'IND' })
+    await City.destroy({
+        where: {
+            id: 3
+        }
+    })
     // Logger.info,Logger.error,Logger.warning
     Logger.info("Sucessfully started server", {});
 });
